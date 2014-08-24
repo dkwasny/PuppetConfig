9b23a966fa347e6b11d866cd274ef6e57dca7190
David Kwasny <dkwasny67@gmail.com>
2014-08-24 18:05:16 -0400

    Yea...rpm update totally works.

--- setup_agent.sh
+++ setup_agent.sh
@@ -2,6 +2,6 @@
 
 # Update all the things and install puppet
 
-sudo rpm -y update;
+sudo yum -y update;
 sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm;
 sudo yum -y install puppet;
