732f77a8b4165f94b923cf2a3880b4d71cd77cf0
dkwasny <dkwasny67@gmail.com>
2014-09-19 20:24:57 -0400

    Adding Solr support.
    In this case, the RPM owns the creation of the var directory due to solr mixing its configuration and var data.

---
+++ environments/production/modules/solr/manifests/init.pp
@@ -0,0 +1,24 @@
+class solr {
+	include java
+	file { "/tmp/solr-4.10.0-1.el7.centos.noarch.rpm":
+		ensure => file,
+		owner => "root",
+		group => "root",
+		mode => 644,
+		source => "puppet:///modules/solr/solr-4.10.0-1.el7.centos.noarch.rpm"
+	} ->
+	# The RPM will add the solr user
+	# and the appropriate systemd unit files.
+	# This particular RPM also adds the appropriate
+	# var directories
+	package { "solr-4.10.0-1.el7.centos.noarch":
+		ensure => present,
+		provider => rpm,
+		source => "/tmp/solr-4.10.0-1.el7.centos.noarch.rpm",
+		allow_virtual => false,
+		require => Class["java"]
+	} ~>
+	service { "solr":
+		enable => true
+	}
+}
